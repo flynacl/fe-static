5994ba51c6287eb2830286e63887fa570bd3e98d
flynacl <1722681724@qq.com>
2019-04-28 18:08:07 +0800

    替换 .babelrc 为 babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+const presets = [
+    [
+        '@babel/preset-env',
+        // {
+        //     targets: {
+        //         edge: '17',
+        //         firefox: '60',
+        //         chrome: '40',
+        //         safari: '11.1',
+        //         ie: '7'
+        //     }
+        // },
+    ],
+];
+
+module.exports = {presets};
